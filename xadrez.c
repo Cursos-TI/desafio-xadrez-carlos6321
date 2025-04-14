7813930fabd945da069f9d2ed4ca54ebbce69f52
carlos6321 <cdiacarlos1978@gmail.com>
2025-04-14 01:04:07 +0000

    criei jogo xadrex

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,31 @@
-#include <stdio.h>
+#include  <stdio.h>
+int main(){
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+int torre = 0, bispo = 0, rainha = 0;
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
+//move a torre 5 casas a direita
+printf("Movendo a Torre\n");
+for (torre = 0; torre <5; torre++) {
+    printf("Direita\n");
 }
+//movendo o bispo 5 casas diagonal á cima e á direita
+printf("\n Movendo o Bispo\n");
+
+while (bispo < 5)
+{
+    printf("Cima, Direita\n");
+    bispo++;
+}
+// movendo a rainha 8 casas a esquerda
+
+printf("Movendo a Rainha\n");
+do{
+printf("Esquerda\n");
+rainha++;
+} while (rainha < 8);
+rainha++;
+return 0;
+
+
+}
+
